ad1b1f9eb647db0285ebce41100410120d33dc8d
Ayo Ogunade <ayotundeogunade13@gmail.com>
2026-01-17 21:46:52 -0600

    added more files!

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true, // <-- This enables the /app router
+  },
 };
 
 export default nextConfig;
